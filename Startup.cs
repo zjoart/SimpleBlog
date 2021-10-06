c93e0a8f8fab922b2257046011714d6be3e4659a
zjoart <oluwadahunsiifeoluwa02@gmail.com>
2021-10-06 17:23:16 +0100

    retracked for deployment

--- Startup.cs
+++ Startup.cs
@@ -46,8 +46,6 @@ namespace SimpleBlog
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
