b97389b6388de6bd6178763d6bde934bba24a446
zjoart <oluwadahunsiifeoluwa02@gmail.com>
2021-10-07 16:41:32 +0100

    'dddd'

--- Startup.cs
+++ Startup.cs
@@ -60,9 +60,9 @@ namespace SimpleBlog
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    // connStr = $"Host={pgHost};Port={pgPort};Database={pgDb};UserID={pgUser}Password={pgPass};sslmode=Prefer;Trust Server Certificate=true";
+                     connStr = $"Server={pgHost};Port={pgPort};Database={pgDb};User Id={pgUser}Password={pgPass};sslmode=Prefer;Trust Server Certificate=true";
                    
-                    connStr = Configuration.GetConnectionString("DeployConnection");
+                   // connStr = Configuration.GetConnectionString("DeployConnection");
                 }
                 options.UseNpgsql(connStr);
 
