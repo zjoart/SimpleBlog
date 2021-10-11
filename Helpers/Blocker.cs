8e0585e823a40dcbda7fa9b9b9cc798079640b94
zjoart <oluwadahunsiifeoluwa02@gmail.com>
2021-10-11 16:06:39 +0100

    Override Login

---
+++ Helpers/Blocker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SimpleBlog.Helpers.Filters
+{
+    public class BlockFilter : IAuthorizationFilter
+    {
+
+        public BlockFilter()
+        {
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            context.Result = new RedirectResult("create"); //Redirect to you desired page
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class BlockAttribute : TypeFilterAttribute
+    {
+        public BlockAttribute() : base(typeof(BlockFilter))
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SimpleBlog.Data;
+using SimpleBlog.Helpers.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
